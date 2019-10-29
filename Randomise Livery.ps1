38dab315b1baed068822463efc625a8f55e723cd
StephenBull41 <46417525+StephenBull41@users.noreply.github.com>
2019-10-30 00:06:03 +1100

    Add files via upload

---
+++ Randomise Livery.ps1
@@ -0,0 +1,6 @@
+﻿$car = gc ([Environment]::GetFolderPath("MyDocuments") + "\Assetto Corsa Competizione\Customs\Cars\test_car.json")
+
+5,6,7,12,13 | % {$car[$_] = $car[$_].Remove($car[$_].IndexOf(':') + 1) + " " + (Get-Random -Minimum 0 -Maximum 359) +","}
+9,10,11,14,15 | % {$car[$_] = $car[$_].Remove($car[$_].IndexOf(':') + 1) + " " + (Get-Random -Minimum 0 -Maximum 5) +","}
+
+$car | Out-File -FilePath ".\test_car.json"
